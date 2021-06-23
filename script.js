35109591cb2a4d0e72ca0e9d5b4c816b3c7fd591
Ivo Kersten <info@studio-yves.com>
2021-06-23 16:49:00 +0200

    Mijn eerste commit vanuit de terminal

---
+++ script.js
@@ -0,0 +1,53 @@
+
+//GET BODY, HAMBURGERMENU EN COLORMENU
+let getBody = document.querySelector('body');
+
+const hamburgermenu = document.querySelector('.hamburgermenu');
+let getColormenu = document.querySelector('.colormenu');
+
+
+//EVENT HAMBURGERMENU
+
+hamburgermenu.addEventListener('click', function(){
+    getColormenu.style.visibility = "visible";
+});
+
+
+//GET BUTTONS IN COLORMENU
+
+const btnGrey = document.querySelector('#btn-grey');
+const btnRed = document.querySelector('#btn-red');
+const btnOrange = document.querySelector('#btn-orange');
+const btnPurple = document.querySelector('#btn-purple');
+const btnGreen = document.querySelector('#btn-green');
+
+//EVENTS BUTTONS IN COLORMENU
+
+btnGrey.addEventListener('click', function(){
+    getBody.style.backgroundColor = "rgb(173, 173, 173)";
+    getColormenu.style.visibility = "hidden";
+});
+
+btnRed.addEventListener('click', function(){
+    getBody.style.backgroundColor = "rgba(212, 0, 0, 0.993)";
+    getColormenu.style.visibility = "hidden";
+});
+
+btnOrange.addEventListener('click', function(toggleColor){
+    getBody.style.backgroundColor = "rgb(255, 123, 0)";
+    getColormenu.style.visibility = "hidden";
+});
+
+btnPurple.addEventListener('click', function(toggleColor){
+    getBody.style.backgroundColor = "rgb(158, 0, 111)";
+    getColormenu.style.visibility = "hidden";
+});
+
+btnGreen.addEventListener('click', function(toggleColor){
+    getBody.style.backgroundColor = "rgb(0, 160, 0)";
+    getColormenu.style.visibility = "hidden";
+});
+
+
+
+
